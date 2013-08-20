187934b77dddfa9005247501cb03de7dbb5966ba
hardsky <hardsky@yandex.ru>
2013-08-20 08:39:43 +0400

    refresh button

--- src/com/hardskygames/habro/HabroActivity.java
+++ src/com/hardskygames/habro/HabroActivity.java
@@ -8,15 +8,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class HabroActivity extends Activity
 {
 	public static RssItem selectedRssItem = null;
-	static String feedUrl = "http://habrahabr.ru/rss/hubs/";
-	ListView rssListView = null;
-	ArrayList<RssItem> rssItems = new ArrayList<RssItem>();
-	ArrayAdapter<RssItem> aa = null;
+	
+	private static String feedUrl = "http://habrahabr.ru/rss/hubs/";
+	private ListView rssListView = null;
+	private ArrayList<RssItem> rssItems = new ArrayList<RssItem>();
+	private ArrayAdapter<RssItem> aa = null;
+	private Button m_btnRefresh = null;
 	
 	public ListView getListView(){
 		return rssListView;
@@ -31,6 +34,7 @@ public class HabroActivity extends Activity
 		
 		// get the listview from layout.xml
 		rssListView = (ListView) findViewById(R.id.lstArticles);
+		m_btnRefresh = (Button) findViewById(R.id.btn_refresh);
 		// here we specify what to execute when individual list items clicked
 		rssListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 		
@@ -40,6 +44,13 @@ public class HabroActivity extends Activity
 				// one page
 				Intent intent = new Intent("com.hardskygames.habro.displayRssItem");
 				startActivity(intent);
+			}
+		});
+		m_btnRefresh.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				refressRssList();
 			}
 		});
 		
